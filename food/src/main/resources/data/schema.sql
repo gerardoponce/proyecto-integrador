eba41c9921ab2cac0b9fa168f4d425414a56435e
gerardoponce <gerardoponcepari@gmail.com>
2020-12-11 17:57:00 -0500

    arreglado para pruebas v2

--- food/src/main/resources/data/schema.sql
+++ food/src/main/resources/data/schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE `membresias` (
   `precio` double NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UK_f2i1wkxqgfdbisdvpxpurjenk` (`nombre`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- food.tipos_platillos definition
 CREATE TABLE `tipos_platillos` (
@@ -24,7 +24,7 @@ CREATE TABLE `tipos_platillos` (
   `nombre` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UK_v128j4setxgobsni2gybelel` (`nombre`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- food.platillos definition
 CREATE TABLE `platillos` (
@@ -39,7 +39,7 @@ CREATE TABLE `platillos` (
   UNIQUE KEY `UK_r68ivrsu3lvs6v1asx3eas2cy` (`foto_platillo`),
   KEY `FK3jeppy3d7ci1nl32i4xhpjk11` (`tipo_platillo`),
   CONSTRAINT `FK3jeppy3d7ci1nl32i4xhpjk11` FOREIGN KEY (`tipo_platillo`) REFERENCES `tipos_platillos` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- food.usuarios definition
 CREATE TABLE `usuarios` (

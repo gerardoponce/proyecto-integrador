5f72c8efe10c6aabe5410853b1785b9b794b7cca
gerardoponce <gerardoponcepari@gmail.com>
2020-12-11 19:47:28 -0500

    arreglado para pruebas v3

--- food/src/main/resources/data/data.sql
+++ food/src/main/resources/data/data.sql
@@ -1,6 +1,6 @@
 USE food;
 
-INSERT INTO membresias(nombre, descripcion, precio) VALUES ("b·sico", "es b·sico y gratuito", 0.00);
+INSERT INTO membresias(nombre, descripcion, precio) VALUES ("b√°sico", "es b√°sico y gratuito", 0.00);
 INSERT INTO membresias(nombre, descripcion, precio) VALUES ("intermedio", "es intermedio y no gratuito", 9.90);
 INSERT INTO membresias(nombre, descripcion, precio) VALUES ("premium", "es premium y no gratuito", 15.90);
 
--- food/src/main/resources/data/schema.sql
+++ food/src/main/resources/data/schema.sql
@@ -1,54 +1,36 @@
-CREATE DATABASE `food` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE food;
 
-USE food;
+use food;
 
-CREATE USER IF NOT EXISTS 'dev'@'localhost' IDENTIFIED BY '#123456#';
-
-GRANT ALL PRIVILEGES ON food.* TO 'dev'@'localhost';
-
-FLUSH PRIVILEGES;
-
--- food.membresias definition
-CREATE TABLE `membresias` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `descripcion` varchar(255) NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `precio` double NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_f2i1wkxqgfdbisdvpxpurjenk` (`nombre`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- food.tipos_platillos definition
-CREATE TABLE `tipos_platillos` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_v128j4setxgobsni2gybelel` (`nombre`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- food.platillos definition
-CREATE TABLE `platillos` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `descripcion` varchar(255) DEFAULT NULL,
-  `foto_platillo` varchar(255) DEFAULT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `precio` double NOT NULL,
-  `tipo_platillo` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_obc6r6wfcbsbdfag1vu6tmtt5` (`nombre`),
-  UNIQUE KEY `UK_r68ivrsu3lvs6v1asx3eas2cy` (`foto_platillo`),
-  KEY `FK3jeppy3d7ci1nl32i4xhpjk11` (`tipo_platillo`),
-  CONSTRAINT `FK3jeppy3d7ci1nl32i4xhpjk11` FOREIGN KEY (`tipo_platillo`) REFERENCES `tipos_platillos` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- food.usuarios definition
-CREATE TABLE `usuarios` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `apellido` varchar(255) DEFAULT NULL,
-  `clave` varchar(255) DEFAULT NULL,
-  `correo` varchar(255) DEFAULT NULL,
-  `direccion` varchar(255) DEFAULT NULL,
-  `foto_perfil` varchar(255) DEFAULT NULL,
-  `nombre` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE membresias (
+  id int(3) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  descripcion varchar(250) NOT NULL,
+  nombre varchar(250) NOT NULL,
+  precio double(6,2) NOT NULL,
+  UNIQUE KEY (nombre)
+);
+CREATE TABLE tipos_platillos (
+  id int(3) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nombre varchar(250) NOT NULL,
+  UNIQUE KEY (nombre)
+);
+CREATE TABLE platillos (
+  id int(3) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  descripcion varchar(250) DEFAULT NULL,
+  foto_platillo varchar(250) DEFAULT NULL,
+  nombre varchar(250) NOT NULL,
+  precio double(6,2) NOT NULL,
+  tipo_platillo int DEFAULT NULL,
+  UNIQUE KEY (nombre),
+  UNIQUE KEY (foto_platillo),
+  FOREIGN KEY (tipo_platillo) REFERENCES tipos_platillos (id)
+);
+CREATE TABLE usuarios (
+  id int(3) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  apellido varchar(250) NOT NULL,
+  clave varchar(250) NOT NULL,
+  correo varchar(250) NOT NULL,
+  direccion varchar(250) not NULL,
+  foto_perfil varchar(250) DEFAULT NULL,
+  nombre varchar(250) NOT NULL
+);
